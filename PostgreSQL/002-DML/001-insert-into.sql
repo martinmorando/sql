bb2e9afcb5bd0b4f908a46d79240d8a58d4731e0
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-28 17:22:24 -0300

    add 002-DML/001-insert-into.sql

---
+++ PostgreSQL/002-DML/001-insert-into.sql
@@ -0,0 +1,19 @@
+/*
+    INSERT INTO
+*/
+
+INSERT INTO movies (title, director, year_published) 
+VALUES
+    ('Raiders of the Lost Ark', 'Steven Spielberg', 1981),
+    ('Mission: Impossible', 'Brian De Palma', 1996),
+    ('Live Free or Die Hard', 'Len Wiseman', 2007),
+    ('Goodfellas', 'Martin Scorsese', 1990),
+    ('The Saint', 'Phillip Noyce', 1997),
+    ('Batman Begins', 'Christopher Nolan', 2005),
+    ('Hackers', 'Iain Softley', 1995),
+    ('The Matrix', 'The Wachowskis', 1999),
+    ('Casino Royale', 'Martin Campbell', 2006),
+    ('The Thomas Crown Affair', 'John McTiernan', 1999),
+    ('Ocean''s Eleven', 'Steven Soderbergh', 2001),
+    ('John Wick', 'Chad Stahelski', 2014),
+    ('Iron Man', 'Jon Favreau', 2008);
