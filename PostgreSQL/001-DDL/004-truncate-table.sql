b8784c2b1c76fd76195520a78212d6fb4878dac1
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-28 17:21:03 -0300

    add PostgreSQL/001-DDL/004-truncate-table.sql and 005-rename-table.sql

---
+++ PostgreSQL/001-DDL/004-truncate-table.sql
@@ -0,0 +1,6 @@
+/*
+    TRUNCATE TABLE
+    - Remove content from database, keeping the structure.
+*/
+
+TRUNCATE TABLE movies;
---
+++ PostgreSQL/001-DDL/005-rename-table.sql
@@ -0,0 +1,6 @@
+/*
+    RENAME TABLE
+    - Change the name of a table.
+*/
+
+ALTER TABLE movies RENAME TO cinema;
