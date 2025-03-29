6a71eb9f19960beaf78a6267c4ce673a273400e8
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-29 19:52:58 -0300

    add PostgreSQL/003-DQL/001-select.sql

---
+++ PostgreSQL/003-DQL/001-select.sql
@@ -0,0 +1,9 @@
+/*
+    Select
+    - Some sources on the internet classify SELECT as DML but 
+      it might make more sense as a DQL.
+    - Pending: DML or DQL?
+*/
+
+-- Basic select
+SELECT title, year_published from movies;
